c52646791950078a1b937664a2bf17ad24264af5
yanni_rist <yanni555rist@gmail.com>
2021-11-25 13:13:37 +1000

    add dumb polling method to wait-for-file

--- src/wait_for_file.rs
+++ src/wait_for_file.rs
@@ -2,25 +2,44 @@ use notify::{DebouncedEvent, watcher, Watcher, RecursiveMode};
 use std::sync::mpsc::channel;
 use std::time::Duration;
 use structopt::StructOpt;
-use std::path::{PathBuf};
+use std::path::{PathBuf, Path};
 use anyhow::Context;
 use path_absolutize::Absolutize;
 
 #[derive(StructOpt)]
 #[structopt(name="wait-for-file", about="Block until a file exists.")]
-/// A convenience wrapper around inotify to wait until a file exists
+/// A convenience wrapper around inotify to wait until a file exists.  You need permission to start an
+/// inotify watch on the closest existing parent directory of the target file.
 struct Args {
   /// Filepath to watch
   filepath: PathBuf,
 
   #[structopt(short="i")]
   /// Wait for an explicit CREATE event, ignoring the file if it existed before `wait-for-file` is run.
-  ignore_existing: bool
+  ignore_existing: bool,
+
+  #[structopt(short="p", value_name="N")]
+  /// Use polling instead of inotify, with a poll interval of N milliseconds
+  poll: Option<u64>
+}
+
+fn poll(path: impl AsRef<Path>, millis: u64) -> anyhow::Result<()> {
+  let path = path.as_ref();
+  let duration = std::time::Duration::from_millis(millis);
+  while !path.exists() {
+    std::thread::yield_now();
+    std::thread::sleep(duration);
+  }
+  Ok(())
 }
 
 fn main() -> anyhow::Result<()> {
   let args: Args = StructOpt::from_args();
   let (tx, rx) = channel();
+
+  if let Some(millis) = args.poll {
+    return poll(&args.filepath, millis)
+  }
 
   let target = args.filepath.absolutize()?;
   let directory = target.ancestors().find(|d| d.is_dir())
